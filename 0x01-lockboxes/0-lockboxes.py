7f7305fb8b58068fc0b68d6549e38a5663d84d6c
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-03-14 06:18:29 +0300

    Practice

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists representing the boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
+    num_boxes = len(boxes)
+    visited = [False] * num_boxes
+    visited[0] = True  # The first box is initially unlocked
+    stack = [0]  # Initialize a stack with the first box
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if 0 <= key < num_boxes and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
+
+# Example usage
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))  # Output: True
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))  # Output: True
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))  # Output: False
