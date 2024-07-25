4afd70efb266114dcb3a54d493bb25f088552863
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-07-25 09:03:33 +0300

    Code Update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,44 +1,30 @@
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding"""
-    # Number of bytes in the current UTF-8 character
-    n_bytes = 0
-
-    # Mask to check if the most significant bit is set
-    mask1 = 1 << 7
-
-    # Mask to check if the second most significant bit is set
-    mask2 = 1 << 6
-
-    # For each integer in the data array.
-    for num in data:
-
-        # Get the binary representation. We only need the least significant 8 bits
-        bin_rep = format(num, '#010b')[-8:]
-
-        # If this is the case then we are to start processing a new UTF-8 character
-        if n_bytes == 0:
-
-            # Get the number of 1s in the beginning of the string.
-            for bit in bin_rep:
-                if bit == '0':
-                    break
-                n_bytes += 1
-
-            # 1 byte characters
-            if n_bytes == 0:
-                continue
-
-            # Invalid scenarios according to the rules of the problem.
-            if n_bytes == 1 or n_bytes > 4:
-                return False
+    """
+    Determines if a given data set represents a valid UTF-8 encoding
+    """
+     # Initialize a variable to keep track of the number of expected bytes
+    expected_bytes = 0
+    
+    # Iterate through the data
+    for byte in data:
+        # Check if the byte is a continuation byte (starts with '10')
+        if byte >> 6 == 0b10:
+            # If it's a continuation byte, decrement the expected_bytes count
+            expected_bytes -= 1
         else:
-
-            # If this is the case then we are processing integers which represent bytes which are a part of a UTF-8 character. So, they must adhere to the pattern `10xxxxxx`.
-            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
-                return False
-
-        # We reduce the number of bytes to process by 1 after each integer.
-        n_bytes -= 1
-
-    # This is for the case where we might not have the complete data for a particular UTF-8 character.
-    return n_bytes == 0
+            # Otherwise, check the number of expected bytes based on the first byte
+            if expected_bytes > 0:
+                return False  # Invalid UTF-8 sequence
+            elif byte >> 7 == 0b0:
+                expected_bytes = 0  # Single-byte character
+            elif byte >> 5 == 0b110:
+                expected_bytes = 1  # Two-byte character
+            elif byte >> 4 == 0b1110:
+                expected_bytes = 2  # Three-byte character
+            elif byte >> 3 == 0b11110:
+                expected_bytes = 3  # Four-byte character
+            else:
+                return False  # Invalid UTF-8 sequence
+    
+    # Check if all expected bytes were consumed
+    return expected_bytes == 0
